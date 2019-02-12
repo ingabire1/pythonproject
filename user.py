967eb096090b6427f424a243865ff689b2ec389b
ingabire <ingabire@sylvie.gmail.com>
2019-02-12 17:36:59 +0200

    changes made on my project

--- user.py
+++ user.py
@@ -9,8 +9,8 @@ class User:
     def save_user(self):
         """Method that saves user objects into user_list"""
         self.user_list.append(self)
+        
+    
 
 
-if __name__ == '__main__':
-    main()
 
