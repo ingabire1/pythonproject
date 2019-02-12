730afc0e1478937685b6f8971c0e0c47ea508566
ingabire <ingabire@sylvie.gmail.com>
2019-02-12 13:07:43 +0200

    python project started

---
+++ user.py
@@ -0,0 +1,52 @@
+
+class User:
+    """
+    Class that generates new instances of user.
+    """
+
+    def __init__(names,phone_number,email):
+
+      # docstring removed for simplicity
+    
+        self.last_name = username
+        self.phone_number =login
+        self.email = password
+
+from hashlib import md5
+from getpass import getpass
+import sys
+
+print("Hello!") 
+
+attempts = 0
+check_username ="A-B-C-D-E-F-G-H-I-J-K-L-M-N-O-P-Q-R-S-T-U-V-W-X-Y-Z"
+check_password = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,"
+# These hashes have been generated earlier on.
+# This is not how you would go about storing usernames and passwords,
+# but for the sake of simplicity, we'll do it like this.
+attempts=0
+while attempts<3:
+    username = input("Username: ")
+    password = getpass("Password: ")
+    # Getpass will not echo input to the screen, so your password remains 
+    # invisible
+    print()
+
+username=input('username?')
+password=input('password?')
+if username=='correctusername'and password=='correctpassword':
+    print('you are in!')
+else:
+    attempts+=1
+    print('incorrect!')
+if attempts==3:
+    print('too many attempts')
+
+    print("Username and password entered correctly.")
+         # Username and password match - do something here
+
+else:
+    print("Username entered incorrectly.")
+    attempts += 1
+
+
---
+++ user_test.py
@@ -0,0 +1,46 @@
+import unittest # Importing the unittest module
+from user import user# Importing the user class
+
+class TestUser(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the user class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+
+    # Items up here .......
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("details","username","password") # create user object
+
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+        self.assertEqual(self.new_details.names,"Muriuki James")
+        self.assertEqual(self.new_username,"0712345678")
+        self.assertEqual(self.new_password,"james@ms.com")
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+ def test_delete_user(self):
+            '''
+            test_delete_user to test if we can remove a user from our user list
+            '''
+            self.new_details.save_details()
+            test_username = Username("Test","user","0712345678","test@user.com") # new user
+            test_passowrd.save_password()
+
+            self.new_details.delete_details()# Deleting a user object
+            self.assertEqual(len(username.username_list),1)
+if __name__ == '__main__':
+    unittest.main()
